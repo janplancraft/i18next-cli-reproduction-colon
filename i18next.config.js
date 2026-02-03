815040a67bb06f41ce4f7e3e46d7c25ab7114bdd
Jan Görgens <jan@plancraft.com>
2026-02-03 13:11:49 +0100

    add other languages

--- i18next.config.js
+++ i18next.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('i18next-cli');
 
 /** @type {import('i18next-cli').I18nextToolkitConfig} */
 module.exports = defineConfig({
-  locales: ["en", "pl"],
+  locales: ["en", "nl", "es", "pl"],
   extract: {
     primaryLanguage: "en",
     input: "src/**/*.{js,jsx,ts,tsx}",
